cc104186d99826c3c5806df987a34b296c34ecfc
sswander <leonardo.sjahputra@kaligo.com>
2023-05-09 18:56:56 +0800

    Update dependencies to fix vulnerabilities
    - urllib3 has to be above 1.26.5
    - joblib has to be above 1.2.0
    - boto3 internally uses urllib3 and will conflict if not updated, so I updated to 1.26.*

--- setup.py
+++ setup.py
@@ -19,15 +19,16 @@ setup(name="pipelinewise-target-redshift",
       py_modules=["target_redshift"],
       install_requires=[
           'pipelinewise-singer-python==1.*',
-          'boto3==1.12.39',
+          'boto3==1.26.*',
           'psycopg2-binary==2.8.5',
           'inflection==0.4.0',
-          'joblib==0.16.0'
+          'joblib>=1.2.0',
+          'urllib3>=1.26.5'
       ],
       extras_require={
           "test": [
                 "pylint==2.4.2",
-                "pytest==5.3.0",
+                "pytest==6.2.5",
                 "mock==3.0.5",
                 "coverage==4.5.4"
             ]
