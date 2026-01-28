12e155663426cd8f065faf534d9490da86016a0f
Quoc Nguyen <quoc.nguyen@ascenda.com>
2026-01-28 17:29:35 +0700

    fix: adding the logger for the error

--- target_redshift/metrics.py
+++ target_redshift/metrics.py
@@ -12,21 +12,25 @@ class MetricsClient:
         self.statsd_prefix = "replication"
         self.statsd_namespace = config.get("metrics", {}).get("statsd_namespace")
         self.statsd_enabled = config.get("metrics", {}).get("statsd_enabled", False)
-        self.statsd_client = None
+        self._statsd_client = None
 
     def _get_statsd_client(self):
-        if self.statsd_enabled and self.statsd_client is None:
-            self.statsd_client = StatsClient(
-                host=self.statsd_host,
-                port=self.statsd_port,
-                prefix=f"{self.statsd_namespace}.{self.statsd_prefix}",
-            )
-        return self.statsd_client
+        if self.statsd_enabled and self._statsd_client is None:
+            try:
+                self.statsd_client = StatsClient(
+                    host=self.statsd_host,
+                    port=self.statsd_port,
+                    prefix=f"{self.statsd_namespace}.{self.statsd_prefix}",
+                )
+            except Exception as exc:
+                logger.error(f"Failed to initialize statsd client: {exc}")
+                return None
+        return self._statsd_client
 
     def _gauge(self, name: str, value: float, tags: Optional[Dict[str, Any]] = None):
-        if self.statsd_enabled:
+        if self.statsd_enabled and (client:=self._get_statsd_client()) is not None:
             logger.info(f"Emitting metric {name} with value {value} and tags {tags}")
-            self._get_statsd_client().gauge(name, value, tags)
+            client.gauge(name, value, tags)
     
     def data_sync_gauge(self, sync_result: dict, stream: str):
         self._gauge(
